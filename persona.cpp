f862d8666997e575eb08fb6e1dbdf0aea701139b
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-16 13:20:37 -0600

    Update persona.cpp

--- persona.cpp
+++ persona.cpp
@@ -1,6 +1,18 @@
 #include "persona.h"
 
+using namespace std;
+
 Persona::Persona()
 {
+    this->nombre ="Pepito";
+    this->rfc ="PEPE564867";
+    this->edad = 8;
+}
+
+void Persona::muestra_datos()
+{
+    cout << "Nombre: " << this->nombre << endl;
+    cout << "Rfc: " << this->rfc << endl;
+    cout << "Edad: " << this->edad << endl;
 
 }
