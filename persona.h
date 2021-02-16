86c535054f96a64847d5c09caf2950e158b972ca
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-16 13:20:41 -0600

    Update persona.h

--- persona.h
+++ persona.h
@@ -1,11 +1,19 @@
 #ifndef PERSONA_H
 #define PERSONA_H
 
+#include <iostream>
+
+using namespace std;
 
 class Persona
 {
 public:
     Persona();
+    void muestra_datos();
+private:
+    string nombre;
+    string rfc;
+    int edad;
 };
 
 #endif // PERSONA_H
