09bd324030fc725dc5bacf24c22e8976aff388c7
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-18 13:26:51 -0600

    Update persona.cpp

--- persona.cpp
+++ persona.cpp
@@ -2,11 +2,18 @@
 
 using namespace std;
 
-Persona::Persona()
+Persona::Persona(string nombre, string rfc, int edad)
 {
-    this->nombre ="Pepito";
-    this->rfc ="PEPE564867";
-    this->edad = 8;
+    this->nombre = nombre;
+    this->rfc = rfc;
+    this->edad = edad;
+}
+
+Persona::Persona(const Persona &persona)
+{
+    this->nombre = persona.nombre;
+    this->rfc = persona.rfc;
+    this->edad = persona.edad;
 }
 
 void Persona::muestra_datos()
@@ -15,4 +22,9 @@ void Persona::muestra_datos()
     cout << "Rfc: " << this->rfc << endl;
     cout << "Edad: " << this->edad << endl;
 
+}
+
+void Persona::muestra_algo(string cadena)
+{
+    cout << "Mostrando: " << cadena << endl;
 }
