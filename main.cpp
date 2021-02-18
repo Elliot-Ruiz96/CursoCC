50ab19307f8aeaa82cb662a0187a96f4c41534ac
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-18 13:27:02 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include "persona.h"
+#include <stdio.h>
+#include "Fecha.h"
+
+void c7_ejemplo();
+void c7_ejemplo2();
 
 int main()
 {
-    Persona Pepito;
-    Pepito.muestra_datos();
-    return 0;
+    void c7_ejemplo();
+    void c7_ejemplo2();
+}
+
+void c7_ejemplo()
+{
+    Persona *Pepito = new Persona("Pepito", "PEPE65874",8);
+    Pepito->muestra_datos();
+    delete Pepito;
+    Persona Elliot("Elliot","RUSE960823",24);
+    Elliot.muestra_datos();
+    Persona::muestra_algo("algo");
+}
+
+void c7_ejemplo2()
+{
+    Fecha Fecha(18, 02, 2021);
+    Fecha.muestra_fecha();
 }
